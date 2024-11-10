22abb01d1e4dc1b00c13bff626afeeb3d8ca0146
PoXel6 <iampoxel6@gmail.com>
2024-11-10 13:22:26 +0000

    I remade my converter with for loop instead of while loop

--- C/C-Programming-Language/chapter1/fahr-to-cels/converter.c
+++ C/C-Programming-Language/chapter1/fahr-to-cels/converter.c
@@ -1,24 +1,10 @@
 #include <stdio.h>
-#include <sys/select.h>
-
 int main() {
-  /*print Fahrenheit-Celsius table
-  for fahr = 0, 20, ..., 300*/
-  float fahr, celsius;
-  float lower, upper, step;
+  int fahr;
+  for (fahr = 0; fahr <= 300; fahr = fahr + 20) {
+    float celsius = (5.0 / 9.0) * (fahr - 32);
 
-  lower = 0;   // lower limit of temperature scale
-  upper = 300; // uppet limit
-  step = 10;   // step size
-
-  celsius = lower;
-
-  printf("A heading Above the Table\n");
-  while (celsius <= upper) {
-    // Formula for coverting fahrenhite to celsius, C=5(fahr-32)/9
-    fahr = celsius * (9.0 / 5.0) + 32;
-
-    printf("%3.0f\t%6.1f\n", celsius, fahr);
-    celsius = celsius + step;
+    printf("%3d %6.1f\n", fahr, celsius);
   }
+  return 0;
 }

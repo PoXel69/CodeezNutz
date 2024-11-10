86dca8dd6e0b7d518db226bd860e739e687595b7
PoXel6 <iampoxel6@gmail.com>
2024-11-10 13:18:00 +0000

    I made my converter better with changing ints to floats

--- C/C-Programming-Language/chapter1/fahr-to-cels/converter.c
+++ C/C-Programming-Language/chapter1/fahr-to-cels/converter.c
@@ -4,8 +4,8 @@ int main() {
   /*print Fahrenheit-Celsius table
   for fahr = 0, 20, ..., 300*/
 
-  int fahr, celsius;
-  int lower, upper, step;
+  float fahr, celsius;
+  float lower, upper, step;
 
   lower = 0;   // lower limit of temperature scale
   upper = 300; // uppet limit
@@ -13,10 +13,12 @@ int main() {
 
   fahr = lower;
 
+  printf("A heading Above the Table\n");
   while (fahr <= upper) {
     // Formula for coverting fahrenhite to celsius, C=5(fahr-32)/9
-    celsius = 5 * (fahr - 32) / 9;
-    printf("%3d\t%6d\n", fahr, celsius);
+
+    celsius = (5.0 / 9.0) * (fahr - 32.0);
+    printf("%3.0f\t%6.1f\n", fahr, celsius);
     fahr = fahr + step;
   }
 }
